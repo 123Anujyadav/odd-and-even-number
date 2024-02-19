dd3d5bedfe9ff0915b391287741f48df273b36a3
123Anujyadav <160490910+123Anujyadav@users.noreply.github.com>
2024-02-19 20:49:01 +0530

    Add files via upload

---
+++ odd_and_even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int odd,even,n;
+    printf("enter the odd and even number");
+    scanf("%d\n",&n);
+    if(n%2==0)
+    printf("the number is odd");
+    if(n%2==1)
+    printf("the number is even");
+    getch();
+    }
+
+
+
+
+}
